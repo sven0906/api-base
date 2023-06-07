4a57e30f7eeec56b072ac466a4e05a44b1ddb0d2
jinhong96 <sven@nolbal.com>
2023-06-07 09:48:44 +0900

    feat: sqlite -> mysql

--- app/stock/admin.py
+++ app/stock/admin.py
@@ -6,11 +6,20 @@ from stock.models import Stock, StockDetail
 class StockAdmin(admin.ModelAdmin):
     list_display = [field.name for field in Stock._meta.fields]
 
+
 @admin.register(StockDetail)
 class StockDetailAdmin(admin.ModelAdmin):
-    list_display = ['id', 'stock', 'get_region', 'link', 'color', 'is_stock', 'created_time', 'modified_time']
+    list_display = [
+        "id",
+        "stock",
+        "get_region",
+        "link",
+        "color",
+        "is_stock",
+        "created_time",
+        "modified_time",
+    ]
 
-    @admin.display(description='stock__region')
+    @admin.display(description="stock__region")
     def get_region(self, obj):
         return obj.stock.region
-
