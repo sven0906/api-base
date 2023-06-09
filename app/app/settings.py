928cf7966d29930854da1cf517c78e03e7eaefac
jinhong96 <sven@nolbal.com>
2023-06-09 13:02:08 +0900

    test: debug true

--- app/app/settings.py
+++ app/app/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-8op9*2k*t334&=$v8wi$7-s6%*n6gqw=ef2nc*y8dbxx=%vk82"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
